cd186851bfd866452ce3114234e93460ed488c04
Nathaniel Pisarski <nathanpisarski@gmail.com>
2016-03-19 01:37:50 -0400

    Generator is confirmed to work

---
+++ MakarovGenerator/MarkovServer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HumDrum.Collections;
+using HumDrum.Collections.Markov;
+
+using HumDrum.Recursion;
+
+using HumDrum.Operations.Files;
+
+namespace MakarovGenerator
+{
+	/// <summary>
+	/// Takes in the text from a directory's files, 
+	/// turning them into states which a markov chain can use.
+	/// </summary>
+	public class MarkovServer
+	{
+		/// <summary>
+		/// The chain generated from the text files
+		/// </summary>
+		/// <value>The chain</value>
+		private Markov<string> Chain {get; set;}
+
+		/// <summary>
+		/// The lock for this server
+		/// </summary>
+		private Object CurrentLock = new Object();
+
+		/// <summary>
+		/// The directory that this server controls
+		/// </summary>
+		/// <value>The directory</value>
+		private string Directory { get; set;}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MakarovGenerator.MarkovServer"/> class.
+		/// Will attempt to read all files in a given directory to build a Markov chain
+		/// </summary>
+		/// <param name="directory">Directory.</param>
+		public MarkovServer (string directory)
+		{
+			Directory = directory;
+			Reload ();
+		}
+
+		/// <summary>
+		/// Reload this instance.
+		/// </summary>
+		public void Reload()
+		{
+			string dataset = "";
+
+			foreach (Line s in new DirectorySearch(Directory, System.IO.SearchOption.TopDirectoryOnly).LinesWhere(x => true))
+				dataset += (s.Text + " ");
+
+			Chain = new Markov<string> (dataset.Split (' '), 1);
+			Console.WriteLine ("Markov Chain successfully generated from source directory");
+		}
+
+		/// <summary>
+		/// Gets a sequence of states from the Markov Chain
+		/// that begin with this state
+		/// </summary>
+		/// <returns>The chain of subsequent states</returns>
+		/// <param name="initialState">The initial state</param>
+		/// <param name="elements">How many elements this chain should generate</param>
+		public IEnumerable<string> GetChain(string initialState, int elements)
+		{
+			lock (CurrentLock) {
+				return Chain.SelectRandomSequence (TailHelper.Wrap (initialState), elements);
+			}
+		}
+
+		/// <summary>
+		/// Gets a sequence of states from a random state
+		/// </summary>
+		/// <returns>The chain of elements</returns>
+		/// <param name="elements">Elements.</param>
+		public IEnumerable<string> GetChain(int elements)
+		{
+			return GetChain (Chain.RandomState ().Get(0), elements);
+		}
+	}
+}
+
--- MakarovGenerator/Program.cs
+++ MakarovGenerator/Program.cs
@@ -5,36 +5,23 @@ using HumDrum.Collections.Markov;
 using HumDrum.Collections.StateModifiers;
 using HumDrum.Collections;
 
+using HumDrum.Recursion;
+
 namespace MakarovGenerator
 {
 	class MainClass
 	{
+		public static void PrintHelp()
+		{
+			
+		}
+
+		// Must be called in the form of "makarov [directory] state"
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Woah buddy. You're running something that won't be ready for a really long time.");
-			Console.WriteLine ("But for other goodies, check out https://www.github.com/natepisarski");
-
-			Console.WriteLine ("But here's some testing data...");
-			Console.WriteLine ("Let's test out some State Objects!");
-			Console.WriteLine ("Wouldn't it be neat to count to 10 with state objects?");
-
-			IntegerCounter x = new IntegerCounter (10, 1);
-			for (; (!x.ModifyState ());) {
-				Console.WriteLine (x.State);
-			}
-
-			Console.WriteLine ();
-			Console.WriteLine ("Now let's see if we can't get elements in pairs of 2 from a big list");
-
-			foreach (List<int> y in Groups.Group<int> (Transformations.Make(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), new IntegerCounter (6, 1))) {
-				y.ForEach (w => Console.Write (w + " "));
-				Console.WriteLine ();
-			}
-
-			Console.WriteLine ("Oh wow, cool! Our stateobjects just kicked some butt.");
-			Console.WriteLine ("Why don't we fire up our Markov library and see if it's working as intended");
-			Markov<string> markov = new Markov<string> (Transformations.Make("here", "lies", "words", "here", "lies", "sounds", "here", "are", "things"), 1);
-			Console.WriteLine ("Based on state of \"lies\" of degree 1: " + markov.SelectRandom (Transformations.Make ("lies")));
+			MarkovServer server = new MarkovServer ("/home/nate/Code/TEST_DIRECTORY");
+			Console.WriteLine(Sections.RepairString(server.GetChain(50)));
+			
 		}
 	}
 }
